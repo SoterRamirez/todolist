4deee8a421d3786e018f6138355bc72f6dbee42c
Soter Ramirez <soterrc@gmail.com>
2022-07-07 13:03:35 -0500

    Manejo del estado en search

--- src/components/TodoSearch/TodoSearch.jsx
+++ src/components/TodoSearch/TodoSearch.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch () {
+    const [searchValue, setSearchValue] = React.useState('');
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     }
 
     return(
-        <input
-            className='TodoSearch'
-            placeholder='Ceboolla'
-            onChange={onSearchValueChange}
-        />
+        <>
+            <input
+                className='TodoSearch'
+                placeholder='Ceboolla'
+                value={searchValue}
+                onChange={onSearchValueChange} />
+            <p> {searchValue} </p>
+        </>
     );
 }
 
