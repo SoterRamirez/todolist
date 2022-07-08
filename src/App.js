45200d96102fe367551d0311f9c63022dd1f284f
Soter Ramirez <soterrc@gmail.com>
2022-07-08 09:28:24 -0500

    Persistencia de datos con Local Storage

--- src/App.js
+++ src/App.js
@@ -6,16 +6,26 @@ import { TodoItem } from "./components/TodoItem/TodoItem";
 import { CreateTodoButton } from "./components/CreateTodoButton/CreateTodoButton";
 import './App.css';
 
-const defaultTodos =[
-  {text: 'Hacer la compra', completed: false},
-  {text: 'Hacer el trabajo', completed: false},
-  {text: 'Hacer el examen', completed: false},
-  {text: 'Hacer el curso', completed: true},
-  {text: 'Hacer la comida', completed:false}
-];
+// const defaultTodos =[
+//   {text: 'Hacer la compra', completed: false},
+//   {text: 'Hacer el trabajo', completed: false},
+//   {text: 'Hacer el examen', completed: false},
+//   {text: 'Hacer el curso', completed: true},
+//   {text: 'Hacer la comida', completed:false}
+// ];
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else{
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState("");
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -32,18 +42,24 @@ function App() {
     })
   }
 
+  const saveTodos = (newTodos) => {
+    const stringTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringTodos);
+    setTodos(newTodos);
+  }
+
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   const createTodo = (text) => {
